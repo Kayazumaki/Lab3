61bef7d25fa4368ff428324897b0e43248ea6a67
Kayazumaki <128226948+Kayazumaki@users.noreply.github.com>
2023-06-17 08:55:57 +0200

    Add files via upload

---
+++ LAB3_06/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3_06
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = GetNumber("Podaj liczbę x: ");
+
+            int n = FindFactorialNumber(x);
+
+            if (n != -1)
+            {
+                Console.WriteLine($"{x} jest silnią liczby {n}.");
+            }
+            else
+            {
+                Console.WriteLine($"{x} nie jest silnią żadnej liczby.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int FindFactorialNumber(int x)
+        {
+            int n = 0;
+            int factorial = 1;
+
+            while (factorial <= x)
+            {
+                if (factorial == x)
+                {
+                    return n;
+                }
+
+                n++;
+                factorial *= n;
+            }
+
+            return -1;
+        }
+
+        static int GetNumber(string message)
+        {
+            int number;
+            while (true)
+            {
+                Console.Write(message);
+                if (int.TryParse(Console.ReadLine(), out number) && number >= 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Błędne dane. Podaj liczbę naturalną.");
+            }
+
+            return number;
+        }
+    }
+}
