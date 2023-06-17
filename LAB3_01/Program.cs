4e7173e746947558c3a6b76cf9fe1c21243df7fe
Kayazumaki <128226948+Kayazumaki@users.noreply.github.com>
2023-06-17 08:41:34 +0200

    Add files via upload

---
+++ LAB3_01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int baseNumber = GetNumber("Podaj podstawę: ");
+            int exponent = GetNumber("Podaj wykładnik: ");
+
+            int result = Power(baseNumber, exponent);
+
+            Console.WriteLine($"{baseNumber} do potęgi {exponent} wynosi {result}.");
+
+            Console.ReadKey();
+        }
+
+        static int Power(int baseNumber, int exponent)
+        {
+            if (exponent == 0)
+                return 1;
+
+            int result = baseNumber;
+
+            for (int i = 1; i < exponent; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+
+        static int GetNumber(string message)
+        {
+            Console.Write(message);
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < 0)
+            {
+                Console.WriteLine("Błędne dane. Podaj liczbę naturalną.");
+                Console.Write(message);
+            }
+
+            return number;
+        }
+    }
+}
